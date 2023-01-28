e30a29b6d2a588ad989f93238205d2fe1329ef37
luis mendoza <luis23luis33@gmail.com>
2023-01-28 08:48:22 -0600

    feat: se crea los componentes de listPlans and Plans

--- src/Components/Plans/ListOfPlans.jsx
+++ src/Components/Plans/ListOfPlans.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Plan from "./Plan";
 
+import './style.css'
+
 function ListOfPlans({ plans }) {
   return (
     <section className="plans">
       {plans.map((plan) => (
-        <Plan plan={plan} />
+        <Plan plan={plan} key={plan.Name} />
       ))}
     </section>
   );
--- src/Components/Plans/Plan.jsx
+++ src/Components/Plans/Plan.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import Img from "../../assets/878719.png"
+
 function Plan({ plan }) {
   return (
     <div className="plan">
-      <div className="info">{plan.Description}</div>
+      <div className="info">
+        <div className="plan-title">
+          <h3>
+          {plan.Name}
+          </h3>
+        </div>
+        <div className="plan-desscription">
+          <p>
+            {plan.Description} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem at nobis aut iure quaerat dolorum beatae, blanditiis esse adipisci quia maiores. Consequatur impedit quo soluta doloribus autem commodi expedita? Quo.
+          </p>
+        </div>
+        </div>
       <div className="image">
-        <img src={plan.img} alt="image" />
+        <img src={Img} alt="image" />
       </div>
     </div>
   );
