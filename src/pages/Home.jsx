92186cf809039d17a32d4b3b0417af96dd19064d
Luis Mendoza <luis23luis33@gmail.com>
2023-01-31 10:37:18 -0500

    feat(error): intentando solucionar ruta del json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   const [plans, setPlans] = useState([]);
 
   const fetchPlans = () => {
-    fetch("../../public/planes.json")
+    fetch("../assets/planes.json")
       .then((res) => res.json())
       .then((plans) => setPlans(plans));
   };
